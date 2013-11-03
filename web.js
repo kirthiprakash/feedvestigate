c85bc08f4c338b063fe2890e8f18cae34ed185e1
kirthi <kp@g4.(none)>
2013-11-04 01:46:03 +0530

    trying to connect to fb

--- web.js
+++ web.js
@@ -6,6 +6,22 @@ app.get('/', function(request, response) {
   response.send('Hello World!');
 });
 
+var Facebook = require('facebook-node-sdk');
+
+app.configure(function () {
+  app.use(express.bodyParser());
+  app.use(express.cookieParser());
+  app.use(express.session({ secret: 'foo bar' }));
+  app.use(Facebook.middleware({ appId: '666840666680350', secret: 'c7a45ca8bd121cd3a543ece4b7bf4dc9' }));
+});
+
+app.get('/lo', Facebook.loginRequired(), function (req, res) {
+  req.facebook.api('/me', function(err, user) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello, ' + user.name + '!');
+  });
+});
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
