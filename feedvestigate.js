3b5551f06c9e107d666b87ffbabb138ba4b11edb
kirthi <kp@g4.(none)>
2013-11-10 20:55:47 +0530

    added until and since

--- feedvestigate.js
+++ feedvestigate.js
@@ -3,9 +3,16 @@ function home(req, res){
 }
 
 function feedvestigate(req, res){
-	var date = req.query.date;
+	var dateStr = req.query.date;
 	console.log(date);
-	var url = '/me/feed?limit=25&until='+date;
+	var date = new Date(dateStr);
+	date.setHours(0);
+	date.setMinutes(0);
+	date.setSeconds(0);
+	date.setMilliseconds(0);
+	date = date/1000;
+	nextDate = date + 86400;
+	var url = '/me/feed?limit=25&until='+nextDate+'&since='+date;
 	req.facebook.api(url, function(err, resp){
 	res.writeHead(200, {'Content-Type':'text/plain'});
 	res.end('url: '+url+' ERROR: '+err+' Resp: '+JSON.stringify(resp));
