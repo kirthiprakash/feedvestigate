15a6476d36008e07c4c12b226969d68404312371
kirthi <kp@g4.(none)>
2013-11-17 05:46:14 +0530

    nothins

--- web.js
+++ web.js
@@ -5,42 +5,53 @@ var ejs = require('ejs');
 var app = express();
 app.use(express.logger());
 
-
-app.configure(function () {
-  app.use(express.bodyParser());
-  app.use(express.cookieParser());
-  app.use(express.session({ secret: 'foo bar' }));
-  app.set('view engine', 'ejs');
-  app.engine('html', ejs.__express);
-  app.use('/steal', express.static(__dirname + '/steal'));
-  app.use('/public', express.static(__dirname + '/public'));
-  app.use(Facebook.middleware({ appId: '666840666680350', secret: 'c7a45ca8bd121cd3a543ece4b7bf4dc9'}));
+app.configure(function() {
+	app.use(express.bodyParser());
+	app.use(express.cookieParser());
+	app.use(express.session({
+		secret : 'foo bar'
+	}));
+	app.set('view engine', 'ejs');
+	app.engine('html', ejs.__express);
+	app.use('/steal', express.static(__dirname + '/steal'));
+	app.use('/public', express.static(__dirname + '/public'));
+	app.use(Facebook.middleware({
+		appId : '666840666680350',
+		secret : 'c7a45ca8bd121cd3a543ece4b7bf4dc9'
+	}));
 });
 
-app.get('/lo', Facebook.loginRequired({scope:'read_stream'}), function (req, res) {
-  req.facebook.api('/me?fields=feed', function(err, user) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello, ' + JSON.stringify(user) + '!'); });
+app.get('/lo', Facebook.loginRequired({
+	scope : 'read_stream'
+}), function(req, res) {
+	req.facebook.api('/me?fields=feed', function(err, user) {
+		res.writeHead(200, {
+			'Content-Type' : 'text/plain'
+		});
+		res.end('Hello, ' + JSON.stringify(user) + '!');
+	});
 });
 
-app.get('/local_feedvestigate', function(req, res){
+app.get('/local_feedvestigate', function(req, res) {
 	var dateStr = req.query.date;
 	console.log(date);
 	var date = new Date(Number(dateStr));
-	console.log(date)
+	console.log(date);
 	date.setHours(0);
 	date.setMinutes(0);
 	date.setSeconds(0);
 	date.setMilliseconds(0);
-	date = date.getTime()/1000;
+	date = date.getTime() / 1000;
 	nextDate = date + 86400;
-	res.end(date+'&'+nextDate);
+	res.end(date + '&' + nextDate);
 });
 
-app.get('/', feedvestigate.home) 
-app.get('/feedvestigate', Facebook.loginRequired({scope:'read_stream'}), feedvestigate.feedvestigate)
+app.get('/', feedvestigate.home);
+app.get('/feedvestigate', Facebook.loginRequired({
+	scope : 'read_stream'
+}), feedvestigate.feedvestigate);
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+	console.log("Listening on " + port);
 });
