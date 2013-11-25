954531a3c37489e034a6221e9254da9f2bbcefd2
kirthi <kp@g4.(none)>
2013-11-26 00:06:51 +0530

    birthday wish modal on homepage

--- steal/ui.js
+++ steal/ui.js
@@ -1,15 +1,12 @@
-var st = steal("http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js", 
-				"public/js/d3.v3.js", 
-				"http://netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.min.css", 
-				"http://netdna.bootstrapcdn.com/bootstrap/3.0.2/js/bootstrap.min.js",
-				"public/css/index.css",
-				"public/calendarPicker/jquery.calendarPicker.js", 
-				"public/calendarPicker/jquery.calendarPicker.css");
-				
+var st = steal("http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js", "public/js/d3.v3.js", "http://netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.min.css", "http://netdna.bootstrapcdn.com/bootstrap/3.0.2/js/bootstrap.min.js", "public/css/index.css", "public/calendarPicker/jquery.calendarPicker.js", "public/calendarPicker/jquery.calendarPicker.css");
+
 st.then(function($) {
 	$(document).ready(function($) {
-		cal = $("#date-select").calendarPicker({callback:function(calen){
-							$('#date-field').val(calen.currentDate.getTime());
-						}});
+		cal = $("#date-select").calendarPicker({
+			callback : function(calen) {
+				$('#date-field').val(calen.currentDate.getTime());
+			}
+		});
+		$('#myModal').modal({});
 	});
 });
