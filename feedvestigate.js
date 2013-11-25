bc3e92015c34a960fef7cb424bd3d60100fe2182
kirthi <kp@g4.(none)>
2013-11-26 03:21:06 +0530

    date alignment

--- feedvestigate.js
+++ feedvestigate.js
@@ -15,7 +15,7 @@ function feedvestigate(req, res) {
 	date.setMilliseconds(0);
 	date = date.getTime() / 1000;
 	// 86400sec -> 1 day
-	nextDate = date + (2 * 86400);
+	nextDate = date + (3 * 86400);
 	data = date - 86400;
 	var patt = /^.*until=(.*)$/;
 	var pageUrlPatt = /^https:\/\/graph.facebook.com\/(\d*)\/feed.*$/;
